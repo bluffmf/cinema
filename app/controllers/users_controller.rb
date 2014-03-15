a47840c0b189d9d434013c5a2f56ee911325517f
bluffmf <bluffmf@gmail.com>
2014-03-15 10:20:56 +0200

    [^]#updating-users fixes in update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 # -*- encoding : utf-8 -*-
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:edit, :update]
+
   
+  def index
+    @users = User.all
+  end
+
   def new
   	@user = User.new
   end
@@ -20,10 +26,30 @@ class UsersController < ApplicationController
   	@user = User.find(params[:id])
   end
 
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit user"
+  end
+
+  def update 
+    @user = User.find(params[:id])
+
+    if @user.update_attributes(user_params)
+      flash[:success] = "Профиль обновлён."
+      redirect_to @user
+    else
+      @title = "Edit user"
+      render 'edit'
+    end
+  end
 
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    def authenticate
+      deny_access unless signed_id?
     end
 end
