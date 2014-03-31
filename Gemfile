c604ee6067d6f1b4447e9bf0948ed4ff274aff0c
bluffmf <bluffmf@gmail.com>
2014-03-31 15:17:23 +0300

    fixes in all

--- Gemfile
+++ Gemfile
@@ -2,8 +2,6 @@ source 'https://rubygems.org'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.0.3'
-gem 'bootstrap-sass', '2.3.2.0'
-gem 'bcrypt-ruby', '3.1.2'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
@@ -12,10 +10,13 @@ gem 'sqlite3'
 gem 'sass-rails', '~> 4.0.0'
 
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '1.3.0'
+gem 'uglifier', '>= 1.3.0'
 
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
+
+# See https://github.com/sstephenson/execjs#readme for more supported runtimes
+# gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
@@ -26,28 +27,30 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+# http://stackoverflow.com/questions/22666681/capistrano-deployment-issue-with-rake
+gem 'rake', '= 10.1.1'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
 end
 
-
-gem 'capistrano', '~> 2.15'
-gem "rvm-capistrano"
-
-
 # Use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.1.2'
 
-# Use unicorn as the app server
-gem 'unicorn'
+group :production do
+  gem 'unicorn'
+end
 
-# Use debugger
-# gem 'debugger', group: [:development, :test]
+group :development do
+  gem 'capistrano'
+  gem 'capistrano-rails'
+  gem 'capistrano-bundler'
+  gem 'capistrano-rvm'
+end
 
-gem 'therubyracer'
+
 gem 'execjs'
+gem 'therubyracer', :platforms => :ruby
 
-gem 'pg', '0.13.2'
-
-gem 'geocoder'
+gem 'pg'
--- config/deploy.rb
+++ config/deploy.rb
@@ -1,163 +1,77 @@
-require "bundler/capistrano"
-require "rvm/capistrano"
+lock '3.1.0'
 
-server "192.168.1.4", :web, :app, :db, primary: true
+set :repo_url, 'git@github.com:bluffmf/cinema'
+set :application, 'cinema'
+application = 'cinema'
+set :rvm_type, :user
+set :rvm_ruby_version, 'ruby-2.1.0@cinema'
 
-set :application, "cinema"
-set :user, "bluffmf"
-set :port, 22
-set :deploy_to, "/home/#{user}/apps/#{application}"
-set :deploy_via, :remote_cache
-set :use_sudo, false
+set :server, :unicorn
 
-set :scm, "git"
-set :repository, "git@github.com:bluffmf/#{application}.git"
-set :branch, "master"
+#set :ssh_options, { :forward_agent => true }
 
-#set :rvm_bin_path, "$HOME/bin"
-set :rvm_path, "$HOME/.rvm"
-#set :default_shell, "/bin/bash -l"
+#set :use_sudo, false
+set :keep_releases, 5
+set :log_level, :debug
 
-set :rvm_ruby_string, 'ruby-2.0.0-p451@cinema' 
+set :linked_dirs, fetch(:linked_dirs, []) + %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
+set :normalize_asset_timestamps, false
 
-default_run_options[:pty] = true
-ssh_options[:forward_agent] = true
+# #on :start do
+# # `ssh-add`
+# #end
 
-after "deploy", "deploy:cleanup" # keep only the last 5 releases
+#SSHKit.config.command_map[:rake]  = "bundle exec rake"
 
-namespace :deploy do
-    %w[start stop restart].each do |command|
-        desc "#{command} unicorn server"
-        task command, roles: :app, except: {no_release: true} do
-            run "/etc/init.d/unicorn_#{application} #{command}"
-        end
-    end
-
-    task :setup_config, roles: :app do
-        sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
-        sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
-        run "mkdir -p #{shared_path}/config"
-        put File.read("config/database.example.yml"), "#{shared_path}/config/database.yml"
-        puts "Now edit the config files in #{shared_path}."
-    end
-    after "deploy:setup", "deploy:setup_config"
-
-
-    task :migrate, roles: :db do
-      run "cd #{current_path} && bundle exec rake db:migrate RAILS_ENV=production"
-    end
-    after "deploy:finalize_update", "deploy:migrate"
-    
-
-    task :symlink_config, roles: :app do
-        run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
-    end
-    after "deploy:migrate", "deploy:symlink_config"
-
-
-    desc "Make sure local git is in sync with remote."
-    task :check_revision, roles: :web do
-        #unless `git rev-parse HEAD` == `git rev-parse origin/master`
-        #    puts "WARNING: HEAD is not the same as origin/master"
-        #    puts "Run `git push` to sync changes."
-        #    exit
-        #end
-    end
-
-    
-
-    before "deploy", "deploy:check_revision"
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-=begin
-
-require 'rvm/capistrano' # Для работы rvm
-require 'bundler/capistrano' # Для работы bundler. При изменении гемов bundler автоматически обновит все гемы на сервере, чтобы они в точности соответствовали гемам разработчика. 
-
-set :application, "cinema"
-set :rails_env, "production"
-set :domain, "bluffmf@192.168.1.5" # Это необходимо для деплоя через ssh. Именно ради этого я настоятельно советовал сразу же залить на сервер свой ключ, чтобы не вводить паролей.
-set :deploy_to, "/srv/#{application}"
-set :use_sudo, false
-set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
-set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
-
-set :rvm_ruby_string, 'ree' # Это указание на то, какой Ruby интерпретатор мы будем использовать.
-
-set :scm, :git # Используем git. Можно, конечно, использовать что-нибудь другое - svn, например, но общая рекомендация для всех кто не использует git - используйте git. 
-set :repository,  "git@github.com:bluffmf/cinema.git" # git@github.com:bluffmf/cinema.git Путь до вашего репозитария. Кстати, забор кода с него происходит уже не от вас, а от сервера, поэтому стоит создать пару rsa ключей на сервере и добавить их в deployment keys в настройках репозитария.
-set :branch, "master" # Ветка из которой будем тянуть код для деплоя.
-set :deploy_via, :remote_cache # Указание на то, что стоит хранить кеш репозитария локально и с каждым деплоем лишь подтягивать произведенные изменения. Очень актуально для больших и тяжелых репозитариев.
-
-role :web, domain
-role :app, domain
-role :db,  domain, :primary => true
-
-before 'deploy:setup', 'rvm:install_rvm', 'rvm:install_ruby' # интеграция rvm с capistrano настолько хороша, что при выполнении cap deploy:setup установит себя и указанный в rvm_ruby_string руби.
-
-after 'deploy:update_code', :roles => :app do
-  # Здесь для примера вставлен только один конфиг с приватными данными - database.yml. Обычно для таких вещей создают папку /srv/myapp/shared/config и кладут файлы туда. При каждом деплое создаются ссылки на них в нужные места приложения.
-  run "rm -f #{current_release}/config/database.yml"
-  run "ln -s #{deploy_to}/shared/config/database.yml #{current_release}/config/database.yml"
-end
-
-# Далее идут правила для перезапуска unicorn. Их стоит просто принять на веру - они работают.
-# В случае с Rails 3 приложениями стоит заменять bundle exec unicorn_rails на bundle exec unicorn
 namespace :deploy do
   task :restart do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D; fi"
+    on roles(:app) do
+      if test("[ -e #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]")
+        execute "kill -USR2 `cat #{fetch(:unicorn_pid)}`"
+      else
+        within release_path do
+          with rails_env: fetch(:rails_env), bundle_gemfile: fetch(:bundle_gemfile) do
+            execute :bundle, "exec unicorn_rails -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D"
+          end
+        end
+      end
+    end
   end
+
   task :start do
-    run "bundle exec unicorn_rails -c #{unicorn_conf} -E #{rails_env} -D"
+    on roles(:app) do
+      within release_path do
+        with rails_env: fetch(:rails_env), bundle_gemfile: fetch(:bundle_gemfile) do
+          execute :bundle, "exec unicorn_rails -c #{fetch(:unicorn_conf)} -E #{fetch(:rails_env)} -D"
+        end
+      end
+    end
   end
+
   task :stop do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
+    on roles(:app) do
+      execute "if [ -f #{fetch(:unicorn_pid)} ] && [ -e /proc/$(cat #{fetch(:unicorn_pid)}) ]; then kill -QUIT `cat #{fetch(:unicorn_pid)}`; fi"
+    end
   end
+
+  task :make_symlink do
+    on roles(:app) do |host|
+      execute "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    end
+  end
+  # config
+  task :copy_conf_files do
+    on roles(:app) do |host|
+      execute "cp -f #{release_path}/config/deploy/#{fetch(:stage)}/unicorn.rb #{release_path}/config/"
+    end
+  end
+
+  after :finishing, "deploy:cleanup"
 end
 
-=end
+before "deploy:updated", "deploy:copy_conf_files"
+before "deploy:updated", "deploy:make_symlink"
+after 'deploy:publishing', 'deploy:restart'
+
+
